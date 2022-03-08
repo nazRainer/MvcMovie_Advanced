0bc6b77fb72d07be7568fe2ad2c04ba020e125e1
User <User@DESKTOP-VEVFTUB>
2022-03-08 17:24:19 +0800

    Add project files.

---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie>? Movies { get; set; }
+        public SelectList? Genres { get; set; }
+        public string? MovieGenre { get; set; }
+        public string? SearchString { get; set; }
+
+        [DataType(DataType.Date)]
+        public SelectList? ReleaseDate { get; set; }
+        [DataType(DataType.Date)]
+        public SelectList? EndDate { get; set; }
+
+        public decimal PriceFilter { get; set; }
+        public decimal PriceFilter2 { get; set; }
+        public SelectList? Price1 { get; set; }
+        public SelectList? Price2 { get; set; }
+
+
+        public string? Title { get; set; }
+    }
+}
